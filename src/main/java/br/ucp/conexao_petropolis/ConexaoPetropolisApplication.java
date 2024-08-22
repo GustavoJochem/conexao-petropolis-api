c10094a6a078065c479b210599483e7d3ccd2be7
GustavoJochem <gustavo.jochem14@hotmail.com>
2024-08-22 11:11:28 -0300

    Initial commit

---
+++ src/main/java/br/ucp/conexao_petropolis/ConexaoPetropolisApplication.java
@@ -0,0 +1,13 @@
+package br.ucp.conexao_petropolis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexaoPetropolisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexaoPetropolisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ucp/conexao_petropolis/ConexaoPetropolisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucp.conexao_petropolis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexaoPetropolisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
